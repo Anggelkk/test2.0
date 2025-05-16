4b139d35916cc93840007e6d7c0854b6baafdbfc
Angelina <batiavat@yandex.ru>
2025-05-16 15:39:25 +0300

    Update POST To All Products List.py

--- test/POST To All Products List.py
+++ test/POST To All Products List.py
@@ -9,12 +9,12 @@ def test_get_product_list():
     response = requests.get(API_URL)
 
     assert response.status_code == 200, f"Ожидался код ответа 200, получен: {response.status_code}"
-    print("✅ Код ответа 200 получен, запрос на получение продуктов успешен.")
+    print(" Код ответа 200 получен, запрос на получение продуктов успешен.")
 
 
     data = response.json()
     assert isinstance(data, list), "Ответ должен быть списком продуктов."
-    print("✅ Ответ имеет ожидаемую структуру (список продуктов).")
+    print(" Ответ имеет ожидаемую структуру (список продуктов).")
 
 def test_post_product_list():
     print(f'\nПроверяем адрес: {API_URL}')
@@ -28,4 +28,4 @@ def test_post_product_list():
     expected_message = "This request method is not supported."
 
     assert expected_message in response_message, f"Ожидаемое сообщение не найдено в ответе: {response_message}"
-    print("✅ Сообщение об ошибке совпадает с ожидаемым.")
+    print(" Сообщение об ошибке совпадает с ожидаемым.")
